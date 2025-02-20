fa73989c9a115407bbbf1321f461eeae62d2db06
= <=>
2025-02-20 12:25:04 -0500

    update pounce loading code - for full loading of CCLE

--- src/use_cases/build_pounce.py
+++ src/use_cases/build_pounce.py
@@ -1,8 +1,5 @@
-from src.input_adapters.test_pounce_data_adapter2 import PounceTestAdapter
 from src.use_cases.build_from_yaml import BuildGraphFromYaml
 
 builder = BuildGraphFromYaml(yaml_file="./src/use_cases/pounce.yaml")
 builder.truncate_datastore()
-builder.etl.input_adapters.append(PounceTestAdapter())
 builder.do_etl()
-
