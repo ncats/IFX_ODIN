d01872c632aa15f54727a18e3a6a6debe1e0ccb0
= <=>
2025-07-19 00:50:23 -0400

    fix some types for ramp

--- src/models/metabolite_chem_props.py
+++ src/models/metabolite_chem_props.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from typing import Optional
+
 from src.models.node import Node
 
 
@@ -6,14 +8,14 @@ from src.models.node import Node
 class MetaboliteChemProps(Node):
     chem_data_source: str = None
     chem_source_id: str = None
-    iso_smiles: str = None
-    inchi_key_prefix: str = None
-    inchi_key: str = None
-    inchi: str = None
-    mw: float = None
-    monoisotop_mass: float = None
-    common_name: str = None
-    mol_formula: str = None
+    iso_smiles: Optional[str] = None
+    inchi_key_prefix: Optional[str] = None
+    inchi_key: Optional[str] = None
+    inchi: Optional[str] = None
+    mw: Optional[float] = None
+    monoisotop_mass: Optional[float] = None
+    common_name: Optional[str] = None
+    mol_formula: Optional[str] = None
 
     def set_id(self):
         filtered_attributes = {k: v for k, v in self.__dict__.items() if not isinstance(v, list)}
