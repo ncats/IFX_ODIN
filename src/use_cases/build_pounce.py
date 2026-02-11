81c6fb5bae992aee02afb1acab59caa535b55c73
kelleherkj <keith.kelleher@ncats.nih.gov>
2026-02-11 14:40:22 -0500

    clean up old pounce parsing and converting code

--- src/use_cases/build_pounce.py
+++ src/use_cases/build_pounce.py
@@ -1,11 +1,5 @@
 from src.use_cases.build_from_yaml import BuildGraphFromYaml
 
-ccle_builder = BuildGraphFromYaml(yaml_file="./src/use_cases/pounce_context.yaml")
-ccle_builder.truncate_datastore()
-ccle_builder.do_etl(do_post_processing=False)
-
-# ccle_builder = BuildGraphFromYaml(yaml_file="./src/use_cases/pounce_ccle.yaml")
-# ccle_builder.do_etl(do_post_processing=False)
-
-ncats_builder = BuildGraphFromYaml(yaml_file="./src/use_cases/pounce_ncats.yaml")
-ncats_builder.do_etl()
+builder = BuildGraphFromYaml(yaml_file="./src/use_cases/pounce.yaml")
+builder.truncate_datastore()
+builder.do_etl()
--- src/use_cases/build_pounce_v2.py
+++
@@ -1,5 +0,0 @@
-from src.use_cases.build_from_yaml import BuildGraphFromYaml
-
-builder = BuildGraphFromYaml(yaml_file="./src/use_cases/pounce_v2.yaml")
-builder.truncate_datastore()
-builder.do_etl()
