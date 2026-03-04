53ceb900d4b4e26d47902d58831e76d547ecc3cf
kelleherkj <keith.kelleher@ncats.nih.gov>
2026-03-04 14:30:09 -0500

    new workflow for translating graph to mysql

---
+++ src/use_cases/convert_graph_pounce_to_mysql.py
@@ -0,0 +1,26 @@
+import yaml
+
+from src.shared.db_credentials import DBCredentials
+from src.use_cases.arango_to_mysql import ArangoToMySqlConverter
+
+arango_credentials_file = "./src/use_cases/secrets/ifxdev_arangodb.yaml"
+mysql_credentials_file = "./src/use_cases/secrets/galeradev_write.yaml"
+minio_credentials_file = "./src/use_cases/secrets/ifxdev_minio.yaml"
+
+with open(arango_credentials_file, "r") as file:
+    arango_credentials = DBCredentials.from_yaml(yaml.safe_load(file))
+
+with open(mysql_credentials_file, "r") as file:
+    mysql_credentials = DBCredentials.from_yaml(yaml.safe_load(file))
+
+with open(minio_credentials_file, "r") as file:
+    minio_credentials = DBCredentials.from_yaml(yaml.safe_load(file))
+
+conv = ArangoToMySqlConverter(
+    arango_credentials=arango_credentials,
+    arango_db_name='pounce',
+    mysql_credentials=mysql_credentials,
+    mysql_db_name='omicsdb_dev2',
+    minio_credentials=minio_credentials)
+
+conv.convert()
